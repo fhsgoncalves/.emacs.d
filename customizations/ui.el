25fdd0e8a3a8ffb5f4ff79b89bd0528094e0d2b9
Fernando <fernando.hsgoncalves@gmail.com>
2015-06-12 23:38:59 -0300

    add new theme: spacegray; change default theme to tomorrow-night-bright

--- customizations/ui.el
+++ customizations/ui.el
@@ -26,7 +26,8 @@
 ;; for a more technical explanation.
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'load-path "~/.emacs.d/themes")
-(load-theme 'darcula t)
+;(load-theme 'darcula t)
+(load-theme 'tomorrow-night-bright t)
 
 ;; increase font size for better readability
 (set-face-attribute 'default nil :height 140)
